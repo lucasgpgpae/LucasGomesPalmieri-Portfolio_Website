e393aa9a6c8eaeffdfd2ec2f3b01631c91681791
Lucas GP <lucasgpgpae@gmail.com>
2019-06-16 16:42:04 -0300

    Wordpress Theme v1.0.1
    Fixed archive in the wrong location!

--- LucasGomesPalmieriPortfolio-THEME/header.php
+++ LucasGomesPalmieriPortfolio-THEME/header.php
@@ -20,7 +20,7 @@ Desenvolvedor: Lucas Gomes Palmieri
 
     <!-- CSS Documents -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/animate.css">
     <!-- End CSS Documents -->
     <!-- JavaScript Documents -->
